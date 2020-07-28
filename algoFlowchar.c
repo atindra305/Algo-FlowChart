a4b076a618cde064830ef48f1d7876f2ecd7d7c6
Atindra Shekhar <56440918+atindra305@users.noreply.github.com>
2020-07-28 16:06:15 +0530

    Final Code
    Final Code

---
+++ algoFlowchar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+
+	double pay;
+
+	double bill;
+	printf("Enter the bill:");
+	scanf("%lf", &bill);
+
+	double tax;
+	tax = bill*0.05;
+
+	double totalBill;
+	totalBill = tax + bill;
+
+	if (totalBill >= 1000){
+		printf("Congratulation! 10% Discount Applied.\n");
+		pay = totalBill - totalBill*0.1;
+	}
+	else{
+		printf(":-( Better Luck Next Time.\n");
+		pay = totalBill;
+	}
+	usleep(200000);
+	printf("You have to pay: %.2lf", pay);
+
+	return 0;
+}
